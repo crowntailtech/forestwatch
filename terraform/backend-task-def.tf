c5a65f0aaa3a6fa83dd7b4ce7e623d6e382325b0
mradityaroy <mradityaroy001@gmail.com>
2025-07-21 02:32:09 +0530

    all files added fixxx

--- terraform/backend-task-def.tf
+++ terraform/backend-task-def.tf
@@ -5,33 +5,41 @@ resource "aws_ecs_task_definition" "backend_task" {
   cpu                      = "256"
   memory                   = "512"
 
-  container_definitions = jsonencode([
+    container_definitions = jsonencode([
     {
-      name      = "backend"
-      image     = "${aws_ecr_repository.backend_repo.repository_url}:latest"
-      essential = true
-      portMappings = [
+        name      = "backend"
+        image     = "${aws_ecr_repository.backend_repo.repository_url}:latest"
+        essential = true
+        portMappings = [
         {
-          containerPort = 5000
-          hostPort      = 5000
+            containerPort = 5000
+            hostPort      = 5000
         }
-      ]
-      environment = [
+        ]
+        environment = [
         {
-          name  = "FLASK_ENV"
-          value = "production"
+            name  = "FLASK_ENV"
+            value = "production"
         },
         {
-          name  = "S3_BUCKET"
-          value = aws_s3_bucket.complaint_images.bucket
+            name  = "S3_BUCKET"
+            value = aws_s3_bucket.complaint_images.bucket
         },
         {
-          name  = "S3_ACCESS_KEY"
-          value = "${var.s3_access_key}"
+            name  = "S3_ACCESS_KEY"
+            value = "${var.s3_access_key}"
         }
-      ]
+        ]
+        logConfiguration = {
+        logDriver = "awslogs"
+        options = {
+            awslogs-group         = "/ecs/${var.student_id}-backend"
+            awslogs-region        = "us-east-1"
+            awslogs-stream-prefix = "ecs"
+        }
+        }
     }
-  ])
+])
 
   execution_role_arn = aws_iam_role.ecs_execution.arn
   task_role_arn      = aws_iam_role.ecs_task_role.arn
