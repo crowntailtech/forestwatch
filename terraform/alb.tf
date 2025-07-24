3ef800d34b055d3ced5d33a73172aa6a5b655712
crowntailtech <139782858+crowntailtech@users.noreply.github.com>
2025-07-24 15:23:24 +0530

    Update alb.tf

--- terraform/alb.tf
+++ terraform/alb.tf
@@ -35,7 +35,7 @@ resource "aws_lb_target_group" "combined_tg" {
   target_type = "ip"
 
   health_check {
-    path                = "/citizen_dashboard.html"
+    path                = "/"
     port                = "traffic-port"
     protocol            = "HTTP"
     matcher             = "200"
